8ae604aa21f87486292ee19833e7eaf60ef2a654
Minh Dao <minh@flowrite.com>
2023-10-15 23:16:59 +0300

    add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chat with PDF",
+  title: "AskPDF",
   description: "Get insights from PDF documents",
 };
 
