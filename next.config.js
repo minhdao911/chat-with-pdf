872ba37dee356c25fdebbbddecd338558ee1f817
Minh Dao <daominh91197@gmail.com>
2025-09-02 03:08:06 +0300

    reduce sentry impact with build time

--- next.config.js
+++ next.config.js
@@ -21,7 +21,10 @@ module.exports = withSentryConfig(module.exports, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+  widenClientFileUpload: false,
+
+  // Only upload source maps in production
+  hideSourceMaps: process.env.NODE_ENV === "production",
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
@@ -31,10 +34,4 @@ module.exports = withSentryConfig(module.exports, {
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 });
